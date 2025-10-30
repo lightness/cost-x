4f2fd8a1602266f201317e3b58601a0517d54df3
Uladzimir Aleshka <uladzimir.aleshka@gmail.com>
2025-10-30 15:44:21 +0300

    WIP

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 import config from './app.config';
 import { CurrencyRateModule } from './currency-rate/currency-rate.module';
 import { DataMigrationModule } from './data-migration/data-migration.module';
@@ -13,6 +15,9 @@ import { TagModule } from './tag/tag.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+    }),
     DatabaseModule,
     TagModule,
     ItemModule,
