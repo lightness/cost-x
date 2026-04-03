b2333d7cc256c77d5efd6c07ad28c40631893a8d
Uladzimir Aleshka <uladzimir.aleshka@gmail.com>
2026-04-03 17:11:31 +0300

    clean(workspace-history): remove unused DTOs

--- src/workspace-history/dto/item-changes.out.dto.ts
+++
@@ -1,8 +0,0 @@
-import { Field, ObjectType } from '@nestjs/graphql';
-import { StringChangeOutDto } from './string-change.out.dto';
-
-@ObjectType()
-export class ItemChangesOutDto {
-  @Field(() => StringChangeOutDto)
-  title: StringChangeOutDto;
-}
