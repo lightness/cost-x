22a8458427e7641305e07606465b29b9303c7e25
Uladzimir Aleshka <uladzimir.aleshka@gmail.com>
2026-04-03 17:11:46 +0300

    fix(workspace-history): remove unused DTO exports

--- src/workspace-history/dto/index.ts
+++ src/workspace-history/dto/index.ts
@@ -1,5 +1,3 @@
-export * from './item-changes.out.dto';
 export * from './on-item-created.event';
 export * from './on-item-deleted.event';
 export * from './on-item-updated.event';
-export * from './string-change.out.dto';
