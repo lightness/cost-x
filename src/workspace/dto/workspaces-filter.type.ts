d04316dc6a8935ab15e52b019bbc0a7edda7ae49
Uladzimir Aleshka <uladzimir.aleshka@gmail.com>
2026-04-03 18:01:27 +0300

    feat(workspace): add id filter to workspaces query

--- src/workspace/dto/workspaces-filter.type.ts
+++ src/workspace/dto/workspaces-filter.type.ts
@@ -1,8 +1,11 @@
-import { Field, InputType } from '@nestjs/graphql';
+import { Field, InputType, Int } from '@nestjs/graphql';
 import { Currency } from '../../currency-rate/entity/currency.enum';
 
 @InputType()
 export class WorkspacesFilter {
+  @Field(() => Int, { nullable: true })
+  id?: number;
+
   @Field(() => String, { nullable: true })
   title?: string;
 
