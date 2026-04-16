39a0fcebd404a78047f5778fb7b3323d8f46d4e7
Uladzimir Aleshka <uladzimir.aleshka@gmail.com>
2026-04-16 16:18:49 +0300

    WIP

--- src/access/strategy/user-to-workspace.access-strategy.ts
+++ src/access/strategy/user-to-workspace.access-strategy.ts
@@ -1,22 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
 import { AccessScope, ResolvedRule } from '../interfaces';
-import { AccessStrategy } from './interface';
 import { GlobalAccessStrategy } from './global.access-strategy';
+import { AccessStrategy } from './interface';
 
 @Injectable()
-export class UserToWorkspaceAccessStrategy
-  extends GlobalAccessStrategy
-  implements AccessStrategy
-{
+export class UserToWorkspaceAccessStrategy extends GlobalAccessStrategy implements AccessStrategy {
+  constructor(private prisma: PrismaService) {
+    super();
+  }
+
   isApplicable(rule: ResolvedRule): boolean {
     return rule.targetScope === AccessScope.WORKSPACE;
   }
 
   async executeRule(rule: ResolvedRule): Promise<boolean> {
-    const userId = (rule.sourceEntity as { id: number }).id;
-    const workspace = rule.targetEntity as { ownerId: number };
+    const currentUser = rule.sourceEntity as { id: number };
+    const workspace = rule.targetEntity as { id: number; ownerId: number };
 
-    if (workspace.ownerId !== userId) {
+    if (workspace.ownerId === currentUser.id) {
+      return super.executeRule(rule);
+    }
+
+    const member = await this.prisma.workspaceMember.findFirst({
+      where: { removedAt: null, userId: currentUser.id, workspaceId: workspace.id },
+    });
+
+    if (!member) {
       return false;
     }
 
